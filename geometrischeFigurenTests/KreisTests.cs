56977155f8e7c14abd483a3c8a07e4ec226d8b33
Vivienne Gschwend <viviennegschwend@gmail.com>
2023-09-07 23:17:29 +0200

    Hinzufuegen Tests

---
+++ geometrischeFigurenTests/KreisTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using geometrischeFiguren;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrischeFiguren.Tests
+{
+    [TestClass()]
+    public class KreisTests
+    {
+        [TestMethod()]
+        public void BerechneFlaecheTest()
+        {
+            double umfang = 10;
+            Kreis kreis = new Kreis(new Linie(umfang));
+            double radius = umfang / (2 * Math.PI);
+            double flaeche = Math.Pow(radius, 2) * Math.PI;
+            Assert.AreEqual(flaeche, kreis.BerechneFlaeche());
+        }
+
+        [TestMethod()]
+        public void BerechneUmfangTest()
+        {
+            double umfang = 23;
+            Kreis kreis = new Kreis(new Linie(umfang));
+            Assert.AreEqual(umfang, kreis.BerechneUmfang());
+        }
+    }
+}
---
+++ geometrischeFigurenTests/RechteckTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using geometrischeFiguren;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrischeFiguren.Tests
+{
+    [TestClass()]
+    public class RechteckTests
+    {
+        [TestMethod()]
+        public void BerechneFlaecheTest()
+        {
+            double linie1 = 15;
+            double linie2 = 10;
+            Rechteck rechteck = new Rechteck(new Linie(linie1), new Linie(linie2));
+            double flaeche = linie1 * linie2;
+            Assert.AreEqual(flaeche, rechteck.BerechneFlaeche());
+        }
+    }
+}
